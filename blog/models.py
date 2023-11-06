06562300d36f4a45249324b4ec89d88128928c67
LAGNER <egorlagner@gmail.com>
2023-11-06 10:09:39 +0300

    добавлен кастомный менеджер published для модели Post

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -17,6 +22,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    objects = models.Manager()  # менеджер по умолчанию
+    published = PublishedManager()  # кастомный менеджер
 
     class Meta:
         ordering = ['-publish']
