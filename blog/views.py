c05229d550eff0040f3d490dc7d7f00f6f3b74ed
LAGNER <egorlagner@gmail.com>
2023-11-08 21:26:58 +0300

    изменено представление post_detail, с использованием get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -11,15 +10,27 @@ def post_list(request):
                   {'posts': posts})
 
 
+# def post_detail(request, year, month, day, slug):
+#     try:
+#         post = Post.cm_published.get(status=Post.Status.PUBLISHED,
+#                                      slug=slug,
+#                                      publish__year=year,
+#                                      publish__month=month,
+#                                      publish__day=day)
+#     except Post.DoesNotExist:
+#         raise Http404("No Post found.")
+#
+#     return render(request,
+#                   'blog/post/detail.html',
+#                   {'post': post})
+
 def post_detail(request, year, month, day, slug):
-    try:
-        post = Post.cm_published.get(status=Post.Status.PUBLISHED,
-                                     slug=slug,
-                                     publish__year=year,
-                                     publish__month=month,
-                                     publish__day=day)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
