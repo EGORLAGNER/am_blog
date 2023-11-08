751d6f0b59bd5bef691e15c9fa94e08633cbeedd
LAGNER <egorlagner@gmail.com>
2023-11-08 11:58:26 +0300

    добавлено представление post_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'post': posts})
