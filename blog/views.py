6e719f36ccb81b3c0b2d994bc9b8fbf129865b5c
LAGNER <egorlagner@gmail.com>
2023-11-08 11:58:53 +0300

    добавлено представление post_detail с использованием Http404

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from blog.models import Post
@@ -8,3 +9,14 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'post': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
