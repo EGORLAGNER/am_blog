59c1e3f5e8e7ff88ba2dd1a82f2695e7a2c058cf
LAGNER <egorlagner@gmail.com>
2023-11-10 12:31:45 +0300

    добавление постраничной разбивки/пагинации для списка постов

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,19 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
 
 def post_list(request):
-    posts = Post.cm_published.all()
+    post_list = Post.cm_published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
-
-# def post_detail(request, year, month, day, slug):
-#     try:
-#         post = Post.cm_published.get(status=Post.Status.PUBLISHED,
-#                                      slug=slug,
-#                                      publish__year=year,
-#                                      publish__month=month,
-#                                      publish__day=day)
-#     except Post.DoesNotExist:
-#         raise Http404("No Post found.")
-#
-#     return render(request,
-#                   'blog/post/detail.html',
-#                   {'post': post})
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post,
