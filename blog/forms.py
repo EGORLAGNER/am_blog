d37b9943cec2d3b34c7514d4b904cb0af2c3163d
LAGNER <egorlagner@gmail.com>
2023-11-11 17:05:28 +0300

    добавлены комментарии к форме EmailPostForm

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+                               widget=forms.Textarea)   # required - обязат поле, widget - переопределяет вид в html
