19bf04398af50e5ff7fb54259e38d428bc8401d8
LAGNER <egorlagner@gmail.com>
2023-11-11 17:24:16 +0300

    добавлено представление post_share для обработки формы EmailPostForm

--- blog/views.py
+++ blog/views.py
@@ -3,24 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from blog.models import Post
+from .forms import EmailPostForm
 
-
-# def post_list(request):
-#     post_list = Post.cm_published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         """Если запрошенная страница вне диапазона страниц, то отрисовать последнюю."""
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         """Если вместо номера страницы в браузере указано не целое число."""
-#         posts = paginator.page(1)
-#
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'posts': posts})
 
 class PostListView(ListView):
     """Существует огромное множество различных View.
@@ -28,9 +12,9 @@ class PostListView(ListView):
      Это поведение по умолчанию переопределяется переменными внутри класса."""
 
     queryset = Post.cm_published.all()  # по умолчанию достаточно передать модель и выполнится запрос Post.objects.all
-    context_object_name = 'posts'   # по умолчанию название переменной object_list
+    context_object_name = 'posts'  # по умолчанию название переменной object_list
     paginate_by = 3
-    template_name = 'blog/post/list.html'   # по умолчанию ищет шаблон 'blog/post_list.html'
+    template_name = 'blog/post/list.html'  # по умолчанию ищет шаблон 'blog/post_list.html'
 
 
 def post_detail(request, year, month, day, slug):
@@ -44,3 +28,26 @@ def post_detail(request, year, month, day, slug):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    # извлечь пост по идентификатору ID
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        # форма передана на обработку
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            # поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # отправить письмо
+    else:
+        # если тип запроса не POST, а следовательно GET, то отобразить пустую форму
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
+
+
