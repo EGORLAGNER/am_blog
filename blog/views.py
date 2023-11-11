977db961c9104b7c09e7e97964053076e8a360e3
LAGNER <egorlagner@gmail.com>
2023-11-11 16:57:19 +0300

    добавлены комментарии к представлению PostListView

--- blog/views.py
+++ blog/views.py
@@ -23,11 +23,14 @@ from blog.models import Post
 #                   {'posts': posts})
 
 class PostListView(ListView):
+    """Существует огромное множество различных View.
+     Они работают по принципу миксинов и по умолчанию обладают определенным поведением.
+     Это поведение по умолчанию переопределяется переменными внутри класса."""
 
-    queryset = Post.cm_published.all()
-    context_object_name = 'posts'
+    queryset = Post.cm_published.all()  # по умолчанию достаточно передать модель и выполнится запрос Post.objects.all
+    context_object_name = 'posts'   # по умолчанию название переменной object_list
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = 'blog/post/list.html'   # по умолчанию ищет шаблон 'blog/post_list.html'
 
 
 def post_detail(request, year, month, day, slug):
