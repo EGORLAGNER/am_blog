07f26f70113a37319ce00e69da9754566931d933
LAGNER <egorlagner@gmail.com>
2023-11-13 15:51:43 +0300

    добавлены настройки для отправки электронных писем

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'egorlagner@gmail.com'
+EMAIL_HOST_PASSWORD = 'mokycjwkpwkdjvoi'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
